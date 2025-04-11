db779dfc201a7d6ac95ff89e1914f8cc88144d81
EDIBERTO CHACON <echacon1638@sdsu.edu>
2025-04-10 19:45:51 -0700

    Changed Class Base Name
    Comments > CommentBase

--- CaffeineCompass/app/comment_schema.py
+++ CaffeineCompass/app/comment_schema.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel
 from datetime import datetime
-class Comments(BaseModel):
+class CommentBase(BaseModel):
     title: str
 
-class Create_Comment(Comments):
+class Create_Comment(CommentBase):
     comment_id: int
 
-class Create_at(Comments):
+class Create_at(CommentBase):
     created_at: datetime 
 
-class Restaurant_id(Comments):
+class Restaurant_id(CommentBase):
     restaurant_id: int
 
-class Tags(Comments):
+class Tags(CommentBase):
     tag_id: int
 
 
     class Config:
         from_attributes = True
         orm_mode = True
-        
+
     
