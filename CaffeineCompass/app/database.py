75a21e0221396b1413c4fdbe0c87884426c591da
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:50:26 -0700

    Update database.py

--- CaffeineCompass/app/database.py
+++ CaffeineCompass/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 # ... (whatever else)
 
-def get_db(): # api endpoints (routes) are created assuming this function exists and is named like this and also that database.py is kept in same location. be sure to change import location of every route if any part of this is altered
+def get_db(): # api endpoints (routes) are created assuming this function exists, is named this, and that 'database.py' is kept in current location. be sure to change import location of every route if any part of this is altered
   #... (whatever else)
