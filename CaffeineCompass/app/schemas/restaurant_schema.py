92392f25c9111922fb8f8702d31bc6168be08ebf
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:15:07 -0700

    Update restaurant_schema.py

--- CaffeineCompass/app/schemas/restaurant_schema.py
+++ CaffeineCompass/app/schemas/restaurant_schema.py
@@ -1,1 +1,22 @@
+from pydantic import BaseModel
+
+class RestaurantBase(BaseModel):
+    address: str
+    name: str
+    rating: float
+    phonenumber: str
+    filtered_tags_id: int
+    comment_id: int
+
+class RestaurantCreate(RestaurantBase):
+    pass
+
+class RestaurantUpdate(RestaurantBase):
+    pass
+
+class Restaurant(RestaurantBase):
+    restaurant_id: int
+
+    class Config:
+        orm_mode = True
 
