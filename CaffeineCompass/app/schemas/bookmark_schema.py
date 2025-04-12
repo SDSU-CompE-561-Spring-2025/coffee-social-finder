0b496e6bc88b290cf11e4a4e2a072046eefa1b36
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:15:36 -0700

    Update bookmark_schema.py

--- CaffeineCompass/app/schemas/bookmark_schema.py
+++ CaffeineCompass/app/schemas/bookmark_schema.py
@@ -1,1 +1,20 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class BookmarkBase(BaseModel):
+    name: str
+    restaurant_name: str
+
+class BookmarkCreate(BookmarkBase):
+    date_bookmarked: datetime
+
+class BookmarkUpdate(BookmarkBase):
+    date_bookmarked: datetime
+
+class Bookmark(BookmarkBase):
+    bookmark_id: int
+    date_bookmarked: datetime
+
+    class Config:
+        orm_mode = True
 
