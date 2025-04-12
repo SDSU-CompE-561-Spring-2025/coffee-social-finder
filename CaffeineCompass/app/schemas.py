b9f6a35fbc0002136fa5f58859c659f422840abf
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:08:45 -0700

    Update schemas.py

--- CaffeineCompass/app/schemas.py
+++ CaffeineCompass/app/schemas.py
@@ -1,13 +1,30 @@
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
+"""
+class UserBase(BaseModel):
+    name: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+        #added a comment new
+"""
+
 class UserBase(BaseModel):
     name: str
     email: EmailStr
 
 class UserCreate(UserBase):
     password: str
-    created_at: datetime
+    created_at: datetime #added this & below for this class
     bookmark_id: int
     filtered_tags_id: int
     cosmetics_id: int
@@ -25,7 +42,7 @@ class UserUpdate(BaseModel):
 
 class User(UserBase):
     id: int
-    created_at: datetime
+    created_at: datetime #added this & below for this class
     bookmark_id: int
     filtered_tags_id: int
     cosmetics_id: int
