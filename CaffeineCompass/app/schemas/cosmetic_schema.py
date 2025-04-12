98e09fdf123f2df0fd838d428876e3ea11fb86fa
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:15:43 -0700

    Update cosmetic_schema.py

--- CaffeineCompass/app/schemas/cosmetic_schema.py
+++ CaffeineCompass/app/schemas/cosmetic_schema.py
@@ -1,1 +1,19 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class CosmeticBase(BaseModel):
+    name: str
+
+class CosmeticCreate(CosmeticBase):
+    date_acquired: datetime
+
+class CosmeticUpdate(CosmeticBase):
+    date_acquired: datetime
+
+class Cosmetic(CosmeticBase):
+    id: int
+    date_acquired: datetime
+
+    class Config:
+        orm_mode = True
 
