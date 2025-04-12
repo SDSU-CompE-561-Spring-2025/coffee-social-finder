d87835875debbffa2810f7fd0f491c1a6748724b
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2025-04-12 05:14:58 -0700

    Update user_schema.py

--- CaffeineCompass/app/schemas/user_schema.py
+++ CaffeineCompass/app/schemas/user_schema.py
@@ -1,1 +1,36 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+    created_at: datetime
+    bookmark_id: int
+    filtered_tags_id: int
+    cosmetics_id: int
+    comment_id: int
+
+class UserUpdate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    created_at: datetime
+    bookmark_id: int
+    filtered_tags_id: int
+    cosmetics_id: int
+    comment_id: int
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+    bookmark_id: int
+    filtered_tags_id: int
+    cosmetics_id: int
+    comment_id: int
+
+    class Config:
+        orm_mode = True
 
