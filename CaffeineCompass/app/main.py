fc84dc6d2fe7230b4d11f1e4677dcd2f34d1dcf2
nimdoble <jlorenzo7270@sdsu.edu>
2025-04-13 03:22:45 -0700

    main.py updated for middleware
    logging requests and handling CORS

--- CaffeineCompass/app/main.py
+++ CaffeineCompass/app/main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi import FastAPI, status, Depends, HTTPException, Request
 from app.core.database import engine, Base, get_db
 from app.routes import auth,user, restaurant, comment, tag, bookmark, cosmetic, address
 from sqlalchemy.orm import Session
+import time
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(restaurant.router)
@@ -13,8 +39,6 @@ app.include_router(tag.router)
 app.include_router(bookmark.router)
 app.include_router(cosmetic.router)
 app.include_router(address.router)
-
-# ... (whatever else)
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def user(token: str = Depends(auth.oauth2_scheme)):
