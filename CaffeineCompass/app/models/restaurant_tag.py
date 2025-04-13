0fcff922dbde4b7cb41ce751d8d1ee639921cef5
Way2Far <wayfaringduck@gmail.com>
2025-04-13 02:19:13 -0700

    JWT Setup

--- CaffeineCompass/app/models/restaurant_tag.py
+++ CaffeineCompass/app/models/restaurant_tag.py
@@ -18,7 +18,7 @@ class Restaurant(Base):
     rating = Column(Integer)
     phoneNumber = Column(String(15))
 
-    tags = relationship("Tag", back_populates = "restaurants")
+    tags = relationship("Tag",secondary= restaurant_tag_association, back_populates = "restaurants")
     comments = relationship("Comment", back_populates = "restaurant")
     bookmarks = relationship("Bookmark", back_populates="restaurant")
 
@@ -28,5 +28,5 @@ class Tag(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), unique = True, nullable= False)
 
-    restaurants = relationship("Restaurant", back_populates = "tags")
+    restaurants = relationship("Restaurant",secondary= restaurant_tag_association, back_populates = "tags")
     comments = relationship("Comment", back_populates = "tags")
