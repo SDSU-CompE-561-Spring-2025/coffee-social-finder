20745c173b43c9a876ed82ee4ea53beaab843284
Way2Far <wayfaringduck@gmail.com>
2025-04-13 02:19:44 -0700

    Revert Main.py

--- CaffeineCompass/app/main.py
+++ CaffeineCompass/app/main.py
@@ -1,23 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.routes import user, restaurant, comment, tag, bookmark, cosmetic, address
-import logging
+from app.routes import auth,user, restaurant, comment, tag, bookmark, cosmetic, address
+
 app = FastAPI()
 
-# Cors middleware setup
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.middleware("http")
-async def logging_middleware(request, call_next):
-    response = await call_next(request)
-    logging.info(f"Request: {request.method} {request.url} - Response: {response.status_code}")
-    return response
 
 app.include_router(user.router)
 app.include_router(restaurant.router)
