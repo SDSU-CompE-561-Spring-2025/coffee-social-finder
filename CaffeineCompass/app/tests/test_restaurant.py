0f7fd52542dd87f57ea2e5e9bc6ea8930d022bcc
filmvince <filmspiredco@gmail.com>
2025-04-13 22:59:48 -0700

    Created test_cosmetic
    adjusted test_restaurant

--- CaffeineCompass/app/tests/test_restaurant.py
+++ CaffeineCompass/app/tests/test_restaurant.py
@@ -1,11 +1,11 @@
 import pytest
 import restaurant_tag
 
-def test_restaurant()
-    pn = Restaurant(id = 1, address = "test", name= "John", rating = 5, phone_number = "6195945200" )
+def test_restaurant():
+    pn = Restaurant(id = 1, address = "test", name= "John", rating = 5, phone_number = "6195945200")
 
     assert pn.id == 1
     assert pn.address == test
     assert pn.name == "John"
     assert pn.rating == 5
-    assert pn. phone_number == "6195945200"
+    assert pn.phone_number == "6195945200"
