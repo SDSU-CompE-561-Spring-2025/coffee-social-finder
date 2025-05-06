100983721de59857d5a4a478054e578c94edbf02
EDIBERTO CHACON <echacon1638@sdsu.edu>
2025-05-06 04:46:14 -0700

    Linked Homepage to Navbar compass image, created page.tsx for forum pagee

--- coffee-social/src/components/Navbar.tsx
+++ coffee-social/src/components/Navbar.tsx
@@ -4,8 +4,10 @@ import Link from "next/link"
 export default function Navbar() {
   return (
     <nav className="bg-[#5D6748] p-1 flex items-center justify-between">
-      <div className="flex items-left">
-        <Image src="/assets/coffeecompass.png" alt="Compass Logo" width={200} height={100} className="mr-2" />
+      <div className="flex justify-start">
+        <Link href="/">
+            <Image src="/assets/coffeecompass.png" alt="Compass Logo" width={200} height={100} className="mr-2" />
+        </Link>
       </div>
       <div className="flex items-center space-x-4 text-white">
         <Link href="/map" className="hover:underline">
@@ -20,9 +22,13 @@ export default function Navbar() {
         <Link href="/profile" className="hover:underline">
           Profile
         </Link>
-        <button className="bg-white text-black px-3 py-1 rounded text-sm">Sign in</button>
-        <button className="bg-[#3a3a3a] text-white px-3 py-1 rounded text-sm">Register</button>
-      </div>
+          <Link href="/sign-in">
+            <button type = "button" className="bg-white text-black px-3 py-1 rounded text-sm">Sign in</button>
+          </Link>
+          <Link href="/Register">
+          <button type ="button" className="bg-[#3a3a3a] text-white px-3 py-1 rounded text-sm">Register</button>
+          </Link>
+          </div>
     </nav>
   )
 }
