db71492af4f5cf8447fc4a67f068afd236cfdf49
EDIBERTO CHACON <echacon1638@sdsu.edu>
2025-05-07 21:27:56 -0700

    ChatGPT Code to be modified for the forum page

--- coffee-social/src/app/forum/page.tsx
+++ coffee-social/src/app/forum/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip"
+// import {
+//     Tooltip,
+//     TooltipContent,
+//     TooltipProvider,
+//     TooltipTrigger,
+// } from "@/components/ui/tooltip"
 
 type CommentData = {
     id: number;
